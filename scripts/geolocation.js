1bb289f7f0e3fa8ddeefd78a933b63c14d2d5075
metalheadvipin <metalheaddude.vipin@gmail.com>
2017-10-09 19:48:44 +0530

    got the object from api

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -1,25 +1,29 @@
-
-          if(navigator.geolocation){
+    $(document).ready(function(){
+      
+      if(navigator.geolocation){
 
             navigator.geolocation.getCurrentPosition(function(position){
 
                 var positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
                 document.getElementById("result").innerHTML = positionInfo;         
-       			var coords = "";
-       	        coords += 'lat=' + position.coords.latitude + '&lon=' + position.coords.longitude; 
-                console.log(coords + 'firsr');         	
+            var coords = "";
+                coords += 'lat=' + position.coords.latitude + '&lon=' + position.coords.longitude; 
+                console.log(coords + 'firsr');          
                 var url = 'https://fcc-weather-api.glitch.me/api/current?';
-				url += coords;
+        url += coords;
 
-				$.getJSON(url, function(data){
-   				  console.log(data);  
-   				  	var result =  getElementById("result");
-   				  	result += "The weather is " + data.main;
-    				});//getJSON
-         		   });
+        $.getJSON(url, function(data){
+            console.log(data);  
+              var result = $("#result");
+              result += "The weather is " + data.main;
+            });//getJSON
+               });
 
-	        
+          
         } else {
-         		   alert("Sorry, your browser does not support HTML5 geolocation.");
+               alert("Sorry, your browser does not support HTML5 geolocation.");
                }
 
+
+    }); 
+          
