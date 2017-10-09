32c424ea39de7d2370e455d00e6e9859209c2376
metalheadvipin <metalheaddude.vipin@gmail.com>
2017-10-09 20:24:47 +0530

    some more

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -1,21 +1,25 @@
     $(document).ready(function(){
-      
+
       if(navigator.geolocation){
 
             navigator.geolocation.getCurrentPosition(function(position){
 
                 var positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
                 document.getElementById("result").innerHTML = positionInfo;         
-            var coords = "";
+                var coords = "";
                 coords += 'lat=' + position.coords.latitude + '&lon=' + position.coords.longitude; 
                 console.log(coords + 'firsr');          
                 var url = 'https://fcc-weather-api.glitch.me/api/current?';
-        url += coords;
+                 url += coords;
 
         $.getJSON(url, function(data){
             console.log(data);  
-              var result = $("#result");
-              result += "The weather is " + data.main;
+            console.log(data.main.temp);  
+            console.log(data.main.humidity);
+            console.log(data.weather.icon);
+
+              var $result = $("#result");
+              $result += "<p> The weather is " + data.main.temp + '&#176; </p>';
             });//getJSON
                });
 
