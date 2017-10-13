e59beedfddd83edd15724dd56cd61c2d35690aa5
metalheadvipin <metalheaddude.vipin@gmail.com>
2017-10-13 15:27:10 +0530

    celcius to fahrenheit not working properly

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -5,30 +5,29 @@
             navigator.geolocation.getCurrentPosition(function(position){
        
                 var coords = "";
-                coords += 'lat=' + position.coords.latitude + '&lon=' + position.coords.longitude; 
-                    
-                var url = 'https://fcc-weather-api.glitch.me/api/current?';
-                 url += coords;
+                coords += 'lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;               
+                var url = 'https://fcc-weather-api.glitch.me/api/current?' + coords;
+                 
 
         $.getJSON(url, function(data){
             console.log(data);  
-            
             var tempinC = Math.round(data.main.temp); 
             var tempinF = Math.round(tempinC * 9/5 + 32);
             console.log(tempinF);
             
-            var resultString = '<h1>' + data.name + ',' + data.sys.country + '</h1>' + '<img src="' + data.weather[0].icon + '">';
-            resultString += '<h2 class="desc">' + data.weather[0].description + '</h2>';
-            resultString += '<h2>Temperature: ' + tempinC + '&#8451;</h2>';
-            resultString +=  '<h2>Humidity: ' + data.main.humidity + '&#x25;</h2>';
+            var result = '<h1>' + data.name + ', ' + data.sys.country + '</h1>' + '<img src="' + data.weather[0].icon + '">';
+            result += '<h2 class="desc">' + data.weather[0].description + '</h2>';
+            result += '<h2 id="temp">Temperature: ' + tempinC + '&#8451;</h2>';
+            result +=  '<h2>Humidity: ' + data.main.humidity + '&#x25;</h2>';
            
             
-            $('#result').append(resultString);
-
+            $('#result').append(result);
+            $('#tempUnit').removeClass("hidden").addClass("display");
 
             
 			$('#tempUnit').click(function(){
- 			
+          $('#temp').html('Temperature: ' + tempinF + '&#8457;');
+ 			  
 				}); // temp button
 	            
             });//getJSON
