085cf3cbed8967d65444dc064e2fce5b5ec952f1
Vipin Sharma <vipin@localhost.localdomain>
2017-10-13 23:13:12 +0530

    finally got the celcius/fahrenheit button working

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -1,26 +1,22 @@
     $(document).ready(function(){
-
-      if(navigator.geolocation){
+      if(navigator.geolocation){  // get user location
 
             navigator.geolocation.getCurrentPosition(function(position){
-       
                 var coords = "";
                 coords += 'lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;               
                 var url = 'https://fcc-weather-api.glitch.me/api/current?' + coords;
                  
 
-        $.getJSON(url, function(data){
-             
+        $.getJSON(url, function(data){ 
             var tempinC = Math.round(data.main.temp); 
             var tempinF = Math.round(tempinC * 9/5 + 32);
 
-                                 
+           	var unit = 'C';     
+
             var result = '<h1>' + data.name + ', ' + data.sys.country + '</h1>' + '<img src="' + data.weather[0].icon + '">';
             result += '<h2 class="desc">' + data.weather[0].description + '</h2>';
             result += '<h2 id="temp">Temperature: ' + tempinC + '&deg;' + unit +'</h2>';
             result +=  '<h2>Humidity: ' + data.main.humidity + '&#x25;</h2>';
-          
-            var unit ='C';
            
             
             $('#result').append(result);
@@ -28,21 +24,21 @@
 
             
 			$('#tempUnit').click(function(){
-        console.log(unit);
-        if (unit == 'C'){
-          $('#temp').html('Temperature: ' + tempinF + '&#8457;');
-           unit == 'F';
-        }
-        else{
-           $('#temp').html('Temperature: ' + tempinF + '&#8457;');
-          
-        }
+			    if (unit == 'C'){
+		        	 unit = 'F';
+		         	 $('#temp').html('Temperature: ' + tempinF + '&deg;' + unit);
+		          
+		       } else { 
+		        	unit = 'C';
+		            $('#temp').html('Temperature: ' + tempinC + '&deg;' + unit);
+		          
+		        }
           			  
-				}); // temp button
-          console.log(unit);
+				}); // temp unit button
+          
 	            
             });//getJSON
-               });
+         });
 
           
         } else {
