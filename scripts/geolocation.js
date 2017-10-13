f1698db4af91eb65aaffc1ddb6cea1d0024b252b
metalheadvipin <metalheaddude.vipin@gmail.com>
2017-10-13 16:56:07 +0530

    still not working properly

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -10,15 +10,17 @@
                  
 
         $.getJSON(url, function(data){
-            console.log(data);  
+             
             var tempinC = Math.round(data.main.temp); 
             var tempinF = Math.round(tempinC * 9/5 + 32);
-            console.log(tempinF);
-            
+
+                                 
             var result = '<h1>' + data.name + ', ' + data.sys.country + '</h1>' + '<img src="' + data.weather[0].icon + '">';
             result += '<h2 class="desc">' + data.weather[0].description + '</h2>';
-            result += '<h2 id="temp">Temperature: ' + tempinC + '&#8451;</h2>';
+            result += '<h2 id="temp">Temperature: ' + tempinC + '&deg;' + unit +'</h2>';
             result +=  '<h2>Humidity: ' + data.main.humidity + '&#x25;</h2>';
+          
+            var unit ='C';
            
             
             $('#result').append(result);
@@ -26,9 +28,18 @@
 
             
 			$('#tempUnit').click(function(){
+        console.log(unit);
+        if (unit == 'C'){
           $('#temp').html('Temperature: ' + tempinF + '&#8457;');
- 			  
+           unit == 'F';
+        }
+        else{
+           $('#temp').html('Temperature: ' + tempinF + '&#8457;');
+          
+        }
+          			  
 				}); // temp button
+          console.log(unit);
 	            
             });//getJSON
                });
